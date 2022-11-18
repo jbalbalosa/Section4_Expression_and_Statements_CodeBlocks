7b0778cf424c00eee41eb35c579d8b5f2ae8adf0
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-18 14:25:02 +0900

    commit and push

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,33 @@ public class Main {
         System.out.println("-----------------------------------------");
 
         boolean gameOver = true;
-        int score = 999;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score < 5000 && score > 1000) {
+/*        if (score < 5000 && score > 1000) {
             System.out.println("Your score was less than 5000 but greater than 1000");
         } else if (score < 1000){
             System.out.println("You score was less than 1000");
         } else {
             System.out.println("Got here");
+        }*/
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Game Over");
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        System.out.println("Second Game!");
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Game Over");
+            System.out.println("Your second final score was " + finalScore);
         }
     }
 }

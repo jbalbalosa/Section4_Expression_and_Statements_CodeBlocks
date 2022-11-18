6a68b7faacfb6a850d350a16c9902f5985b41102
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-18 11:44:46 +0900

    commit and push

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Expressions and Statements");
+        System.out.println("Code Blocks");
+    }
+}
